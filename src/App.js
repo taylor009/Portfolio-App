ebcd12ec4bd171077439a11b40e74807193d83d6
taylor <taylor@baremetalcloud.com>
2020-02-07 00:50:59 -0500

    added about section and change sidenav icon sizes

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
             <div id="colorlib-main">
               <Introduction/>
               <About/>
+            {/*  todo*/}
             {/*  <Timeline></Timeline>*/}
             </div>
           </div>
