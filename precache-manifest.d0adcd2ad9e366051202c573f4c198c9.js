dd049d3ebfaa8e3e7a904ae48c96e45347420625
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-12 08:41:07 +0000

    Deploy taylor009/Portfolio-App to github.com/taylor009/Portfolio-App.git:gh-pages

---
+++ precache-manifest.d0adcd2ad9e366051202c573f4c198c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac7ec4e55784e5ad622913788660683",
+    "url": "/Portfolio-App/index.html"
+  },
+  {
+    "revision": "3b175d52debbf9bbb704",
+    "url": "/Portfolio-App/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "743e05d92e964c1233c3",
+    "url": "/Portfolio-App/static/js/2.3c27fc57.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Portfolio-App/static/js/2.3c27fc57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b175d52debbf9bbb704",
+    "url": "/Portfolio-App/static/js/main.76aa2c4b.chunk.js"
+  },
+  {
+    "revision": "d6b50e96114868ad1be5",
+    "url": "/Portfolio-App/static/js/runtime-main.91873985.js"
+  }
+]);
